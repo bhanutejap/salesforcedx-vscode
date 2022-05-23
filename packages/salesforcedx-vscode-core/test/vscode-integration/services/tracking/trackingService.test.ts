05433e35b812b74d60872e5e8989a8dea939d9e6
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-05-23 10:07:40 -0700

    test: add first test not passing

--- packages/salesforcedx-vscode-core/test/vscode-integration/services/tracking/trackingService.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/services/tracking/trackingService.test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, salesforce.com, inc.
+ * Copyright (c) 2022, salesforce.com, inc.
  * All rights reserved.
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
@@ -15,13 +15,20 @@ import { statusSummaryString } from './testdata/statusSummaryStrings';
 // tslint:disable:no-unused-expression
 describe('getSourceStatusSummary', () => {
   it('Should return a formatted string when local and remote changes exist', async () => {
-    const stubSourceTracking = sinon.createStubInstance(SourceTracking);
-    stubSourceTracking.getStatus.returns(statusRowsData);
-    const output: string = await TrackingService.instance.getSourceStatusSummary(
-      {}
+    // Arrange
+    const data = statusRowsData;
+    const sourceTrackingStub = sinon.createStubInstance(SourceTracking);
+    sourceTrackingStub.getStatus.returns(statusRowsData);
+    const trackingServiceSUT: TrackingService = new TrackingService(
+      sourceTrackingStub
     );
+    // expect(TrackingService._tracking).to.equal(sourceTrackingStub);
 
-    console.log(statusRowsData);
+    // Act
+    const output: string = await trackingServiceSUT.getSourceStatusSummary({});
+
+    // Assert
+    // console.log(statusRowsData);
     console.log(output);
   });
 });
