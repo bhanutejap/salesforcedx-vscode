37c3e1b54065cb993fb1df87c6c51c316aaab79e
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-05-23 17:03:12 -0700

    feat: update test

--- packages/salesforcedx-vscode-core/test/vscode-integration/services/tracking/trackingService.test.ts
+++ packages/salesforcedx-vscode-core/test/vscode-integration/services/tracking/trackingService.test.ts
@@ -8,13 +8,12 @@
 import { SourceTracking } from '@salesforce/source-tracking';
 import { expect } from 'chai';
 import * as sinon from 'sinon';
-import { nls } from '../../../../src/messages';
 import { TrackingService } from '../../../../src/services/trackingService';
 import { statusRowsData } from './testdata/statusRowsData';
 import { statusSummaryString } from './testdata/statusSummaryStrings';
-// tslint:disable:no-unused-expression
+
 describe('getSourceStatusSummary', () => {
-  it('Should return a formatted string when local and remote changes exist', async () => {
+  it('Should return a properly formatted string when local and remote changes exist.', async () => {
     // Arrange
     const sourceTrackingStub = sinon.createStubInstance(SourceTracking);
     sourceTrackingStub.getStatus.returns(statusRowsData);
