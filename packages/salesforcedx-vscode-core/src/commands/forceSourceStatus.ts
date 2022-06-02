bcee0b298ff7ece872b14d27c0874841f7253e78
Kenneth Lewis <kenneth.lewis@salesforce.com>
2022-06-02 12:15:01 -0700

    refactor: remove unneeded imports

--- packages/salesforcedx-vscode-core/src/commands/forceSourceStatus.ts
+++ packages/salesforcedx-vscode-core/src/commands/forceSourceStatus.ts
@@ -4,15 +4,13 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
 import { LibraryCommandletExecutor } from '@salesforce/salesforcedx-utils-vscode/out/src';
 import {
   Command,
   SfdxCommandBuilder
 } from '@salesforce/salesforcedx-utils-vscode/out/src/cli';
 import { ContinueResponse } from '@salesforce/salesforcedx-utils-vscode/src/types';
-// import { LibraryCommandletExecutor } from '@salesforce/salesforcedx-utils-vscode/src';
-// import { ContinueResponse } from '@salesforce/salesforcedx-utils-vscode/src/types';
-// import { CancellationToken, Progress } from 'vscode';
 import { channelService, OUTPUT_CHANNEL } from '../channels';
 import { nls } from '../messages';
 import { SourceTrackingService } from '../services/sourceTrackingService';
